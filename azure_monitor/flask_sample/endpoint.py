c0dd5b7c9bc37f05ed63cd7dffcfb84ab7cb36ec
Paul Muther <PaulMuther@users.noreply.github.com>
2026-01-21 19:10:06 -0500

    Update endpoint.py

--- azure_monitor/flask_sample/endpoint.py
+++ azure_monitor/flask_sample/endpoint.py
@@ -1,3 +1,4 @@
 from endpoint import endpoint_app
 
-endpoint_app.run(host='localhost', port=5001, threaded=True)
+if __name__ == '__main__':
+    endpoint_app.run(host='localhost', port=5001, threaded=True, debug=True)
